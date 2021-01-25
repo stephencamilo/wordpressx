003d908fc08496110a47563b8b8e08c963ec248a
Stephen Edurado Camilo <stephen.camilo@live.com>
2021-01-25 14:52:37 -0300

    good version

--- core/wp-includes/Requests/Exception/Transport.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Requests_Exception_Transport extends Requests_Exception
-{
-
-}
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
 define('WP_USE_THEMES', true);
 
-if (!defined('ABSPATH')) {
-    define('ABSPATH', dirname(__DIR__) . '/');
+if (! defined('ABSPATH')) {
+    define('ABSPATH', dirname(__DIR__) . $_SERVER["REQUEST_URI"]);
 }
 
-if (!defined('ABSPATH_CORE')) {
-    define('ABSPATH_CORE', dirname(__DIR__) . '/core/');
+if (! defined('ABSPATH_CORE')) {
+    define('ABSPATH_CORE', dirname(__DIR__) . $_SERVER["REQUEST_URI"] . 'core/backend/');
 }
 
-require ABSPATH_CORE . '/wp-blog-header.php';
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/core/backend/wp-blog-header.php';
