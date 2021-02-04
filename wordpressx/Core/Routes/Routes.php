59bf3cc4b3b105b2333bee794b4ffb3239a2c749
Stephen Edurado Camilo <stephen.camilo@live.com>
2021-02-04 20:07:13 -0300

    Adding routes to WP :3

---
+++ wordpressx/Core/Routes/Routes.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Core\Routes;
+
+class Routes {
+    function init_router() {
+        $klein = new \Klein\Klein;
+
+        $klein->respond('GET', '/', function () {
+            $WPX_index = new \Core\Wordpressx\Index;
+            $WPX_index->blog_header();
+        });
+
+        $klein->respond('GET', '/admin/setup/config', function () {
+            $WPX_setup_config = new \Core\WPAdmin\SetupConfig;
+            $WPX_setup_config->blog_header();
+        });
+        
+        $klein->respond('GET', '/hello-world', function () {
+            return 'Hello World!';
+        });
+        
+        $klein->dispatch();
+    }
+}
